2b01d107bec84091aea94d7f5e0bb928bc6e62b7
rsbars <144855925+rsbars@users.noreply.github.com>
2023-09-17 18:41:22 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios;
+
+
+public class Ejercicio2 {
+
+
+    public static void main(String[] args) {
+        
+        String nombre="Brian Rosales", direccion="Lima", telefono="934233323";
+        
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Dirección: "+direccion);
+        System.out.println("Teléfono: "+telefono);
+        
+    }
+    
+}
